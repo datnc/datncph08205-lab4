450ed6824b765210611883cf2576f65588cf9911
datnc <hipboylc@gmail.com>
2020-03-25 22:47:00 +0700

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AboutComponent } from './about/about.component';
-import { AddProductComponent } from './add-product/add-product.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { CategoryComponent } from './category/category.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ProductService } from './service/product.service';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { ServiceComponent } from './service/service.component';
-import { ProductService } from './product.service';
-import { SlideComponent } from './slide/slide.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AboutComponent, AddProductComponent, CategoryComponent, HomeComponent, NotFoundComponent, ProductComponent, ProductDetailComponent, ProductListComponent, ProductManagerComponent, ServiceComponent, SlideComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent, ProductManagerComponent, AddProductComponent, AboutComponent, HomeComponent, NotFoundComponent, ProductDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
