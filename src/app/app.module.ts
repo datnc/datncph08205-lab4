f5cca1dca565d9347e41b5517f23a1ad01bdf335
datnc <hipboylc@gmail.com>
2020-03-25 22:20:40 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ServiceComponent } from './service/service.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, AddProductComponent, CategoryComponent, HomeComponent, NotFoundComponent, ProductComponent, ProductDetailComponent, ProductListComponent, ProductManagerComponent, ServiceComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
